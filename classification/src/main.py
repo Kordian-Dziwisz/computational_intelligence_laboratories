e649da6d57584cf837b9024a9481f8ed99970a1d
kordian <k.dziwisz.635@studms.ug.edu.pl>
2024-03-06 15:01:41 +0100

    Implemented function to find missing values

--- classification/src/main.py
+++ classification/src/main.py
@@ -1,11 +1,15 @@
 import pandas as pd
+import numpy as np
 from pathlib import Path
 
+def get_empty_fields(df):
+    print(df.isnull().sum())
 
 
 def main():
     df = pd.read_csv(f"{Path.cwd()}/assets/iris_with_errors.csv")
-    print(df.head())
+    # print(df.head())
+    get_empty_fields(df)
     pass
 
 if __name__ == "__main__":
